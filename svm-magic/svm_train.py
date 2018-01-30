31f879a0ebfddcc557d62d80523bf29425ecfce2
Priyansh Trivedi <pc.priyansh@gmail.com>
2018-01-30 15:06:38 +0100

    Random sampling in trainset

--- svm-magic/svm_train.py
+++ svm-magic/svm_train.py
@@ -1,12 +1,24 @@
 import pickle
+import numpy as np
 from sklearn import svm
+
+np.random.seed(42)
 
 data = pickle.load(open('matrices.dat'))
 clf = svm.SVC(decision_function_shape='ovo', verbose=True, max_iter=1, cache_size=3000)
 
 print("About to start training")
 
-clf.fit(data['trainX'], data['trainY'])
+trainX = data['trainX']
+trainY = data['trainY']
+
+# Now select samples from it like a ba-mofo
+indices = np.random.choice(np.arange(len(trainX)), len(trainX)/10)
+trainX = [ trainX[i] for i in indices]
+trainY = [ trainY[i] for i in indices]
+
+
+clf.fit(trainX, trainY)
 answers = clf.predict(data['testX'])
 
 pickle.dump(answers, open('answers.dat', 'w+'))
